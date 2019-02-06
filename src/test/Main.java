a2a2cd97b5ec4160d7253b4930dc401f7b6aae0c
Serhii Ivko <sergii.ivko@gmail.com>
2019-02-06 18:48:58 +0200

    change class main

--- src/test/Main.java
+++ src/test/Main.java
@@ -4,15 +4,11 @@ public class Main {
 
     public static void main(String[] args) {
         int v = 6;
-        boolean isPowerOfTwo = v && !(v && (v - 1));
-//        System.out.println("Hello World!");
-//        String s = "1" + "2" + "2" + "2" + "2" + "2" + "2" + "2" + "2" + "2" + 5 + "2";
-//        String d = "1".concat("2").concat("3").concat("4").concat(5);
-//        boolean ddd = true;
-//        long e = 5L;
-//        switch (3){
-//
-//        }
+//        boolean isPowerOfTwo = v && !(v && (v - 1));
+        System.out.println("Hello World!");
+        String s = "1" + "2" + "2" + "2" + "2" + "2" + "2" + "2" + "2" + "2" + 5 + "2";
+        String d = "1".concat("2").concat("3").concat("4").concat("5");
+
         System.out.println(getChar(25));
     }
 
